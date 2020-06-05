3b88dcd1a4baaa09f70d0d469d3bf1084af1d2d7
mynameisORIG <57204473+mynameisORIG@users.noreply.github.com>
2020-06-05 08:56:38 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from tkinter import *
+from tkinter import ttk
+from john_the_ripper_GUI import JohnTheRipper
+import os, subprocess as sub, platform  
+#from goBuster import goBuster
+ 
+JohnTheRipper() 
+#goBuster()
